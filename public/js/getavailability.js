8e7484d63441c8e32fd9e8dcd1702445c92a054c
Adam Shire <shirea@wit.edu>
2018-06-29 15:19:15 -0400

    add wttech endpoint

--- public/js/getavailability.js
+++ public/js/getavailability.js
@@ -4,17 +4,11 @@ function getItemStatusFromMfhdIds(mfhdIds) {
 
 	$.post('/mfhd', {mfhdIDs: mfhdIds }) //send array of mfhds to our post route for mfhds
 	.done(function(data) { // take the response from the API and update the UI
-		updateItemStatus(data);
+		  //updateItemStatus(data)
+			console.log(data);
+			return data;
 	})
 	.fail(function() {
 		alert ("error");
-	});
-}
-
-function updateItemStatus(data) { // update the UI with item availability
-	$.each(data, function (index, item) {
-		console.log(item);
-		var status = item.STATUS == 1 ? "Checked Out" : "Available";
-		$('[mfhd_id="'+item.MFHD_ID+'"]').append(`<li>itemID: ${index} : status ${status}</li>`);
 	});
 }
